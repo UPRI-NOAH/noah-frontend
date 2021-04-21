96a7516cae75584dd0f1512fc4d9f55d7b1766ec
bon <jccarpo@up.edu.ph>
2021-04-21 22:32:58 +0800

    Add Back and Next button in sidebar for pages

--- src/app/features/personalized-risk-assessment/personalized-risk-assessment-routing.module.ts
+++ src/app/features/personalized-risk-assessment/personalized-risk-assessment-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BaseComponent } from './components/base/base.component';
+import { FloodComponent } from './components/flood/flood.component';
 
 const routes: Routes = [
   {
     path: 'base',
     component: BaseComponent,
+  },
+  {
+    path: 'flood',
+    component: FloodComponent,
   },
 ];
 
