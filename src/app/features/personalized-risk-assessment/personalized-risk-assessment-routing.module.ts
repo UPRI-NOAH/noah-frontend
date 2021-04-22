edd1841f7dd720bcc9db6048c7ba1ddacff940d3
bon <jccarpo@up.edu.ph>
2021-04-22 19:14:32 +0800

    Add PRA link to all pages Side navigation drop shadow Add static info for every pages

--- src/app/features/personalized-risk-assessment/personalized-risk-assessment-routing.module.ts
+++ src/app/features/personalized-risk-assessment/personalized-risk-assessment-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BaseComponent } from './components/base/base.component';
+import { CriticalFacilitiesComponent } from './components/critical-facilities/critical-facilities.component';
 import { FloodComponent } from './components/flood/flood.component';
+import { LandslideComponent } from './components/landslide/landslide.component';
+import { StormSurgeComponent } from './components/storm-surge/storm-surge.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,18 @@ const routes: Routes = [
   {
     path: 'flood',
     component: FloodComponent,
+  },
+  {
+    path: 'landslide',
+    component: LandslideComponent,
+  },
+  {
+    path: 'storm-surge',
+    component: StormSurgeComponent,
+  },
+  {
+    path: 'critical-facilites',
+    component: CriticalFacilitiesComponent,
   },
 ];
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,19 @@ module.exports = {
       },
       colors: {
         primary: 'var(--color-primary)',
+        low: 'var(--color-low)',
+        medium: 'var(--color-medium)',
+        high: 'var(--color-high)',
         'light-blue': colors.lightBlue,
         cyan: colors.cyan,
       },
+      boxShadow: ['active'],
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      alpha: 'lower-alpha',
     },
   },
   variants: {
