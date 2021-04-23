f4554e31dccf018484f677afa161e600ffeb4255
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-04-23 10:36:21 +0800

    added base pra component

---
+++ src/app/features/personalized-risk-assessment/components/pra-map/pra-map.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-pra-map',
+  templateUrl: './pra-map.component.html',
+  styleUrls: ['./pra-map.component.scss'],
+})
+export class PraMapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/features/personalized-risk-assessment/personalized-risk-assessment.module.ts
+++ src/app/features/personalized-risk-assessment/personalized-risk-assessment.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// MODULES
 import { PersonalizedRiskAssessmentRoutingModule } from './personalized-risk-assessment-routing.module';
+
+// PAGES / CONTAINERS
+import { PersonalRiskComponent } from './pages/personal-risk/personal-risk.component';
+
+// COMPONENTS
 import { BaseComponent } from './components/base/base.component';
 import { FloodComponent } from './components/flood/flood.component';
 import { LandslideComponent } from './components/landslide/landslide.component';
 import { StormSurgeComponent } from './components/storm-surge/storm-surge.component';
 import { CriticalFacilitiesComponent } from './components/critical-facilities/critical-facilities.component';
-import { PersonalRiskComponent } from './pages/personal-risk/personal-risk.component';
+import { PraMapComponent } from './components/pra-map/pra-map.component';
 
 @NgModule({
   declarations: [
+    PersonalRiskComponent,
     BaseComponent,
     FloodComponent,
     LandslideComponent,
     StormSurgeComponent,
     CriticalFacilitiesComponent,
-    PersonalRiskComponent,
+    PraMapComponent,
   ],
   imports: [CommonModule, PersonalizedRiskAssessmentRoutingModule],
 })
