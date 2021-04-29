f2df9a84bff1bd637571337f449ad15176de06ab
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-04-29 11:25:55 +0800

    aded mock critical facilities

--- src/app/features/personalized-risk-assessment/components/critical-facilities/critical-facilities.component.ts
+++ src/app/features/personalized-risk-assessment/components/critical-facilities/critical-facilities.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PraService } from '@features/personalized-risk-assessment/services/pra.service';
+import { MARKERS, SampleMarker } from '@shared/mocks/critical-facilities';
 
 @Component({
   selector: 'noah-critical-facilities',
@@ -7,9 +8,15 @@ import { PraService } from '@features/personalized-risk-assessment/services/pra.
   styleUrls: ['./critical-facilities.component.scss'],
 })
 export class CriticalFacilitiesComponent implements OnInit {
+  criticalFacilities = MARKERS;
+
   constructor(private praService: PraService) {}
 
   ngOnInit(): void {
     this.praService.setCurrentPage('critical-facilities');
+  }
+
+  focus(marker: SampleMarker) {
+    console.log({ marker });
   }
 }
