f6da9f29f533485bccd10afc1542443a5846ed4e
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-05-31 12:49:34 +0800

    flood 100-year return period

--- src/app/shared/mocks/flood.ts
+++ src/app/shared/mocks/flood.ts
@@ -71,3 +71,29 @@ export const LEYTE_FLOOD_25: FillLayer = {
     'fill-opacity': 1,
   },
 };
+
+// This is a duplicate of LEYTE_FLOOD at the top of this file
+// This is a workaround. We'll refactor this later on.
+export const LEYTE_FLOOD_100: FillLayer = {
+  id: 'flood-100',
+  type: 'fill',
+  source: {
+    type: 'vector',
+    url: 'mapbox://jadurani.3tg2ae87',
+  },
+  'source-layer': 'Leyte_Flood_100year-15dhkm',
+  paint: {
+    'fill-color': [
+      'interpolate',
+      ['linear'],
+      ['get', 'Var'],
+      1,
+      '#f2c94c',
+      2,
+      '#f2994a',
+      3,
+      '#eb5757',
+    ],
+    'fill-opacity': 1,
+  },
+};
