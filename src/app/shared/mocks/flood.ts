c740e2b11c3d7a3b3b3deabb45a5fa3459f7fcda
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-05-31 12:40:40 +0800

    add 5-year and 25-year for abuyog, leyte

--- src/app/shared/mocks/flood.ts
+++ src/app/shared/mocks/flood.ts
@@ -23,3 +23,51 @@ export const LEYTE_FLOOD: FillLayer = {
     'fill-opacity': 1,
   },
 };
+
+export const LEYTE_FLOOD_5: FillLayer = {
+  id: 'flood-5',
+  type: 'fill',
+  source: {
+    type: 'vector',
+    url: 'mapbox://jadurani.bhiuk4sy',
+  },
+  'source-layer': 'drive-download-20210531T01250-2t4e1m',
+  paint: {
+    'fill-color': [
+      'interpolate',
+      ['linear'],
+      ['get', 'Var'],
+      1,
+      '#f2c94c',
+      2,
+      '#f2994a',
+      3,
+      '#eb5757',
+    ],
+    'fill-opacity': 1,
+  },
+};
+
+export const LEYTE_FLOOD_25: FillLayer = {
+  id: 'flood-25',
+  type: 'fill',
+  source: {
+    type: 'vector',
+    url: 'mapbox://jadurani.3e5y5216',
+  },
+  'source-layer': '25-leyte-abuyog-60lan2',
+  paint: {
+    'fill-color': [
+      'interpolate',
+      ['linear'],
+      ['get', 'Var'],
+      1,
+      '#f2c94c',
+      2,
+      '#f2994a',
+      3,
+      '#eb5757',
+    ],
+    'fill-opacity': 1,
+  },
+};
