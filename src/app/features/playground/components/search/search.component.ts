0a8f702397a7635240f5a1fe9232e1b33ce35b2c
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-06-14 14:28:36 +0800

    custom search component

--- src/app/features/playground/components/search/search.component.ts
+++ src/app/features/playground/components/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MapService } from '@core/services/map.service';
+import { BehaviorSubject } from 'rxjs';
+// import { Feature, FeatureCollection, GeoJsonProperties, Geometry } from '@mapbox/geojson-types';
+import { debounceTime, filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'noah-search',
@@ -8,11 +12,30 @@ import { FormControl } from '@angular/forms';
 })
 export class SearchComponent implements OnInit {
   searchTermCtrl: FormControl;
+  places$: BehaviorSubject<any[]>;
 
-  constructor() {}
+  isDropdownOpen = false;
+
+  constructor(private mapService: MapService) {}
 
   ngOnInit(): void {
     this.searchTermCtrl = new FormControl();
-    this.searchTermCtrl.valueChanges.subscribe((value) => console.log(value));
+    this.places$ = new BehaviorSubject([]);
+
+    this.searchTermCtrl.valueChanges
+      .pipe(
+        debounceTime(800),
+        filter((searchText) => searchText && this.isDropdownOpen),
+        switchMap((searchText) => this.mapService.forwardGeocode(searchText))
+      )
+      .subscribe((value: any) => {
+        this.places$.next(value.features);
+      });
+  }
+
+  selectPlace(place) {
+    this.searchTermCtrl.setValue(place.text);
+    this.isDropdownOpen = false;
+    // add another control here
   }
 }
