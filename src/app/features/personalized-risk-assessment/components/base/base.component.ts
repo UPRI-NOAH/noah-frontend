f7b628d3b865caef9117bf040a410b99e1e419e9
kennethbeoliporada <kennethbeoliporada@gmail.com>
2021-06-22 18:54:43 +0800

    WIP: UI for PHA

--- src/app/features/personalized-risk-assessment/components/base/base.component.ts
+++ src/app/features/personalized-risk-assessment/components/base/base.component.ts
@@ -8,6 +8,10 @@ import { Observable } from 'rxjs';
   styleUrls: ['./base.component.scss'],
 })
 export class BaseComponent implements OnInit {
+  isFlood: boolean = false;
+  islandSlide: boolean = false;
+  isStorm: boolean = false;
+
   currentLocation$: Observable<string>;
 
   constructor(private praService: PraService) {}
