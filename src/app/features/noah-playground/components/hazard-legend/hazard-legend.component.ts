c1bec70773f235afcf410d7c4ef93e6050b6853a
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-07-02 22:45:34 +0800

    updated colors; working color switcher

--- src/app/features/noah-playground/components/hazard-legend/hazard-legend.component.ts
+++ src/app/features/noah-playground/components/hazard-legend/hazard-legend.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NOAH_COLORS_ARRAY } from '@shared/mocks/noah-colors';
 
 @Component({
   selector: 'noah-hazard-legend',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hazard-legend.component.scss'],
 })
 export class HazardLegendComponent implements OnInit {
+  noahColors: string[] = NOAH_COLORS_ARRAY;
+  levels: string[] = ['high', 'medium', 'low'];
+  selectedColor = 'noah-red';
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.noahColors);
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,37 @@ module.exports = {
         color3: 'var(--color-3)',
         color4: 'var(--color-4)',
         color5: 'var(--color-5)',
+
+        'noah-pink': {
+          low: '#FDACB2',
+          medium: '#FA5F96',
+          high: '#C21D7D',
+        },
+        'noah-red': {
+          low: '#F2C94C',
+          medium: '#F2994A',
+          high: '#EB5757',
+        },
+        'noah-violet': {
+          low: '#A9C6DE',
+          medium: '#818ABC',
+          high: '#804B9B',
+        },
+        'noah-blue': {
+          low: '#B4D1E2',
+          medium: '#5FA3CE',
+          high: '#2B75B2',
+        },
+        'noah-green': {
+          low: '#B7E392',
+          medium: '#66BF71',
+          high: '#1A994E',
+        },
+        'noah-black': {
+          low: '#DADBDB',
+          medium: '#8B8B8B',
+          high: '#333333',
+        },
       },
       boxShadow: ['active'],
     },
