96c7244fe7ba28dd86405fa4985fb0a690516b3f
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-07-02 16:38:21 +0800

    moved footer out of navigation component

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { FooterComponent } from './components/navigation/footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { PersonalizedRiskAssessmentModule } from '@features/personalized-risk-assessment/personalized-risk-assessment.module';
 import { PlaygroundModule } from '@features/playground/playground.module';
 
