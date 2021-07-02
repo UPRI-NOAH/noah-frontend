7d097f845931b05f86852b96924e542caf2d196f
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-07-03 00:06:34 +0800

    componentized hazard types

--- src/app/features/noah-playground/noah-playground.module.ts
+++ src/app/features/noah-playground/noah-playground.module.ts
@@ -14,6 +14,7 @@ import { SearchPlaygroundComponent } from './components/search-playground/search
 import { SliderComponent } from './components/slider/slider.component';
 import { HazardLegendComponent } from './components/hazard-legend/hazard-legend.component';
 import { HazardLevelComponent } from './components/hazard-level/hazard-level.component';
+import { HazardTypeComponent } from './components/hazard-type/hazard-type.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { HazardLevelComponent } from './components/hazard-level/hazard-level.com
     SliderComponent,
     HazardLegendComponent,
     HazardLevelComponent,
+    HazardTypeComponent,
   ],
   imports: [CommonModule, NoahPlaygroundRoutingModule, SharedModule],
 })
