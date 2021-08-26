b00251ed6ed19bf6aa8f68237820021834e1a626
kennethbeoliporada <kennethbeoliporada@gmail.com>
2021-08-26 18:42:23 +0800

    for directive i forget to commit and push

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchComponent } from './components/search/search.component';
+import { ArrowKeysDirective } from './arrow-keys.directive';
 
 const modules = [CommonModule, FormsModule, ReactiveFormsModule];
 const components = [SearchComponent];
 
 @NgModule({
-  declarations: [...components],
+  declarations: [...components, ArrowKeysDirective],
   imports: [...modules],
   exports: [...modules, ...components],
 })
