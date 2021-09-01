a50a44338e8e4b805bb76762d226c9a0c6d83899
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-09-01 17:03:43 +0800

    added sensor service

--- src/app/features/noah-playground/services/sensor.service.ts
+++ src/app/features/noah-playground/services/sensor.service.ts
@@ -1,8 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export type SensorType =
+  | 'arg' // Auomated Rain Gauge
+  | 'wlms' // Water LEvel Monitoring System
+  | 'aws' // Automated Weather Stations
+  | 'wlmsarg'; // Waterlevel & Rain
 
 @Injectable({
   providedIn: 'root',
 })
 export class SensorService {
-  constructor() {}
+  private BASE_URL = 'http://202.90.159.72:8000';
+
+  constructor(private http: HttpClient) {}
+
+  getSensors(type: SensorType) {
+    const param = type ? `?sensor_type=${type}` : '';
+    return this.http.get(`${this.BASE_URL}/api/sensors/${param}`);
+  }
+
+  getSensorData(pk: number) {
+    return this.http.get(`${this.BASE_URL}/api/sensor-data/?sensor=${pk}`);
+  }
 }
