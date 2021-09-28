37a30bdc47becb3dff60ae7623a6fa5a1a232a07
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-09-28 21:35:56 +0800

    updated cluster colors

--- src/app/shared/mocks/critical-facilities.ts
+++ src/app/shared/mocks/critical-facilities.ts
@@ -118,24 +118,25 @@ export const getSymbolLayer = (sourceName: string): SymbolLayer => ({
   },
 });
 
-export const getCircleLayer = (sourceName: string): CircleLayer => ({
-  id: `${sourceName}-cluster`,
-  type: 'circle',
-  source: sourceName,
-  filter: ['has', 'point_count'],
-  paint: {
-    'circle-color': [
-      'step',
-      ['get', 'point_count'],
-      '#51bbd6',
-      100,
-      '#f1f075',
-      750,
-      '#f28cb1',
-    ],
-    'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
-  },
-});
+export const getCircleLayer = (sourceName: string): CircleLayer => {
+  const circleColors: Record<CriticalFacility, string> = {
+    'fire-station': '#FEC7A5',
+    hospital: '#F5C3DF',
+    'police-station': '#C4CFE4',
+    school: '#A7DEC7',
+  };
+
+  return {
+    id: `${sourceName}-cluster`,
+    type: 'circle',
+    source: sourceName,
+    filter: ['has', 'point_count'],
+    paint: {
+      'circle-color': circleColors[sourceName],
+      'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
+    },
+  };
+};
 
 export const getClusterTextCount = (sourceName: string): SymbolLayer => {
   const facilityNames: Record<CriticalFacility, string> = {
