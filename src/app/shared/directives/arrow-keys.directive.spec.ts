723c0636c99156c9a4fd56d8a3ecb2b70c24ace2
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-09-29 23:10:48 +0800

    cleanups

--- src/app/shared/directives/arrow-keys.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ArrowKeysDirective } from './arrow-keys.directive';
-
-describe('ArrowKeysDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ArrowKeysDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchComponent } from './components/search/search.component';
-import { ArrowKeysDirective } from './directives/arrow-keys.directive';
 
 const modules = [CommonModule, FormsModule, ReactiveFormsModule];
 const components = [SearchComponent];
-const directives = [ArrowKeysDirective];
+const directives = [];
 @NgModule({
   declarations: [...components, ...directives],
   imports: [...modules],
