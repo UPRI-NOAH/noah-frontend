f3322786cc5d7cac44b4000878508c74c1cd0ddf
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-09-29 22:26:06 +0800

    working current location

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -6,16 +6,14 @@ import {
   OnInit,
   Output,
   QueryList,
-  Renderer2,
   ViewChildren,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MapService } from '@core/services/map.service';
-import { KyhService } from '@features/know-your-hazards/services/kyh.service';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
-import { ArrowKeysDirective } from '@shared/directives/arrow-keys.directive';
 import { ENTER } from '@angular/cdk/keycodes';
+import { LocationService } from '@core/services/location.service';
 
 @Component({
   selector: 'noah-search',
@@ -25,33 +23,24 @@ import { ENTER } from '@angular/cdk/keycodes';
 export class SearchComponent implements OnInit {
   @Input() searchTerm: string;
   @Output() selectPlace: EventEmitter<any> = new EventEmitter();
-  currentLocation$: Observable<string>;
   searchTermCtrl: FormControl;
   places$: BehaviorSubject<any[]>;
 
   isDropdownOpen = false;
   isCurrent = false;
 
-  row: number = 0;
   focusedRowIdx: number = 0;
-  // @ViewChildren(ArrowKeysDirective) inputs: QueryList<ArrowKeysDirective>;
+
   @ViewChildren('locationOptions') locationOptions: QueryList<ElementRef>;
 
   constructor(
-    private kyhService: KyhService,
     private mapService: MapService,
-    private renderer: Renderer2
+    private locationService: LocationService
   ) {}
 
   ngOnInit(): void {
-    this.kyhService.keyBoard.subscribe((res) => {
-      this.moveByArrowkey(res);
-    });
-
-    this.kyhService.init();
     this.searchTermCtrl = new FormControl(this.searchTerm);
     this.places$ = new BehaviorSubject([]);
-    this.currentLocation$ = this.kyhService.currentLocation$;
 
     this.searchTermCtrl.valueChanges
       .pipe(
@@ -111,35 +100,21 @@ export class SearchComponent implements OnInit {
   }
 
   async userFixedLocation() {
-    // HERE GETTING USER COORDINATES THEN PARSE TO LOCAL STORAGE
-    function locationSuccess(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      const coords = { lng: longitude, lat: latitude };
-      localStorage.setItem('userLocation', JSON.stringify(coords));
-    }
-
-    function locationError(error) {
-      const message = error.message;
-      alert(message);
-    }
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
-    //GETTING COORDINATES TO LOCAL STORAGE
-    const user = localStorage.getItem('userLocation');
-    const userCoords = JSON.parse(user);
+    const coords: { lat: number; lng: number } =
+      await this.locationService.getCurrentLocation();
 
     const userPlaceName = await this.mapService.reverseGeocode(
-      userCoords.lat,
-      userCoords.lng
+      coords.lat,
+      coords.lng
     );
-    localStorage.setItem('userPlaceName', userPlaceName); //storing place name in localstorage
+
     const selectedPlace = {
       text: userPlaceName,
-      center: [userCoords.lng, userCoords.lat],
+      center: [coords.lng, coords.lat],
     };
+
     this.selectPlace.emit(selectedPlace);
     this.searchTermCtrl.setValue(userPlaceName);
-    this.kyhService.setCenter(userCoords); // user location center when in kyh page
   }
 
   onEnterUser(event) {
@@ -150,7 +125,7 @@ export class SearchComponent implements OnInit {
         text: localStorage.getItem('userPlaceName'),
         center: [userCoords.lng, userCoords.lat],
       };
-      this.kyhService.setCenter(userCoords); // user location center when in kyh page
+
       this.selectPlace.emit(selectedPlace);
       this.searchTermCtrl.setValue(localStorage.getItem('userPlaceName'));
     }
@@ -160,26 +135,5 @@ export class SearchComponent implements OnInit {
     this.searchTermCtrl.setValue(place.text);
     this.isDropdownOpen = false;
     this.selectPlace.emit(place);
-  }
-
-  moveByArrowkey(object) {
-    // const inputToArray = this.inputs.toArray();
-    // let index = inputToArray.findIndex((x) => x.element == object.element);
-    // switch (object.action) {
-    //   case 'UP':
-    //     index -= this.row;
-    //     clearInterval(object);
-    //     break;
-    //   case 'DOWN':
-    //     index += this.row;
-    //     clearInterval(object);
-    //     break;
-    //   case 'ENTER':
-    //     index == this.row;
-    //     clearInterval(object);
-    // }
-    // if (index >= 0 && index < this.inputs.length) {
-    //   inputToArray[index].element.nativeElement.focus();
-    // }
   }
 }
