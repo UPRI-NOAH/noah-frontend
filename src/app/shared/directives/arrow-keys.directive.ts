540e05c285cf3b408053b5674a5cb39d4b32923e
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-09-29 17:28:02 +0800

    declare event type

--- src/app/shared/directives/arrow-keys.directive.ts
+++ src/app/shared/directives/arrow-keys.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { KyhService } from '@features/know-your-hazards/services/kyh.service';
-import { UP_ARROW, DOWN_ARROW, ENTER } from '@angular/cdk/keycodes';
+
 @Directive({
   selector: '[noahArrowKeys]',
 })
@@ -13,17 +13,17 @@ export class ArrowKeysDirective {
     this.render.setAttribute(this.element.nativeElement, 'tabindex', '0');
   }
 
-  @HostListener('keydown', ['$event']) onKeyup(e) {
-    switch (e.keyCode) {
-      case UP_ARROW:
+  @HostListener('keydown', ['$event']) onKeyup(e: KeyboardEvent) {
+    switch (e.code) {
+      case 'ArrowUp':
         this.kyhService.sendMessage({ element: this.element, action: 'UP' });
         e.preventDefault();
         break;
-      case DOWN_ARROW:
+      case 'ArrowDown':
         this.kyhService.sendMessage({ element: this.element, action: 'DOWN' });
         e.preventDefault();
         break;
-      case ENTER:
+      case 'Enter':
         this.kyhService.sendMessage({ element: this.element, action: 'ENTER' });
         break;
     }
