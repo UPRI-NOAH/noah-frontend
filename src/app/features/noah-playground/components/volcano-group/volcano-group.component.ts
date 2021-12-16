f929dac7bdf3844d783a703974ef46200e5674b1
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-12-17 07:31:49 +0800

    working set up

---
+++ src/app/features/noah-playground/components/volcano-group/volcano-group.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-volcano-group',
+  templateUrl: './volcano-group.component.html',
+  styleUrls: ['./volcano-group.component.scss'],
+})
+export class VolcanoGroupComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/features/noah-playground/components/volcano-solo/volcano-solo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-volcano-solo',
+  templateUrl: './volcano-solo.component.html',
+  styleUrls: ['./volcano-solo.component.scss'],
+})
+export class VolcanoSoloComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
