6b2fb522e74d5f8843c6c44580da7bdf3896713a
Jadurani Davalos <jadurani.davalos@gmail.com>
2022-01-13 17:48:13 +0800

    feat(studio): display volcanoes (#244)
    # Summary
    
    - Allow the user to view and change the opacity of the following volcano types
      - active
      - potentially active
      - inactive
    - Data sourced from s3
      - Active - https://upri-noah.s3.ap-southeast-1.amazonaws.com/volcanoes/active_volcano.geojson
      - Potentially Active - https://upri-noah.s3.ap-southeast-1.amazonaws.com/volcanoes/volcanoes_potentially_active.geojson
      - Inactive - https://upri-noah.s3.ap-southeast-1.amazonaws.com/volcanoes/volcanoes_inactive.geojson
    - Labels and elevation are displayed below the icons
      - Elevation is in `meters` from the data
      - If elevation isn't available, it's not displayed
    - Volcano names are "text-optional", meaning it won't be displayed if it collides with any other map element but the icon will still be displayed
    
    # Screenshots
    
    ![image](https://user-images.githubusercontent.com/11599005/149305755-a0cbee43-a3d9-4815-a575-678314f06b22.png)
    
    ![image](https://user-images.githubusercontent.com/11599005/149305783-a0274562-7cb8-4677-9a9f-aaae8cda9a6d.png)
    
    ![image](https://user-images.githubusercontent.com/11599005/149305814-eaa76b84-fd7b-4dbc-9258-d377a3ba4c00.png)
    
    ![image](https://user-images.githubusercontent.com/11599005/149305859-29666278-3a16-4cc8-9378-ff812f59069f.png)
    
    ![image](https://user-images.githubusercontent.com/11599005/149305944-9f6d232f-dc83-4a22-96b4-8ac9e295bf5f.png)
    
    # Related links
    
    https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions/

---
+++ src/app/features/noah-playground/components/volcano-solo/volcano-solo.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NoahPlaygroundService } from '@features/noah-playground/services/noah-playground.service';
+import { VolcanoType } from '@features/noah-playground/store/noah-playground.store';
+import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+@Component({
+  selector: 'noah-volcano-solo',
+  templateUrl: './volcano-solo.component.html',
+  styleUrls: ['./volcano-solo.component.scss'],
+})
+export class VolcanoSoloComponent implements OnInit {
+  @Input() volcanoType: VolcanoType;
+
+  initialOpacityValue: number = 75;
+  shown = false;
+
+  constructor(private pgService: NoahPlaygroundService) {}
+
+  ngOnInit(): void {
+    this.pgService
+      .getVolcano$(this.volcanoType)
+      .pipe(first())
+      .subscribe(({ shown, opacity }) => {
+        this.shown = shown;
+        this.initialOpacityValue = opacity;
+      });
+  }
+
+  changeOpacity(opacity: number) {
+    this.pgService.setVolcanoSoloOpacity(opacity, this.volcanoType);
+  }
+
+  toggleShown() {
+    this.shown = !this.shown;
+    this.pgService.setVolcanoSoloShown(this.shown, this.volcanoType);
+  }
+}
