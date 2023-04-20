d4f523faf4fe66d714127d49a753a157520e3635
kennethliporada <62271587+kennethliporada@users.noreply.github.com>
2023-04-20 11:14:43 +0800

    created a risk assessment services

--- src/app/features/noah-playground/services/risk-assessment-services.service.ts
+++ src/app/features/noah-playground/services/risk-assessment-services.service.ts
@@ -1,22 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-export type RiskAssessmentType =
-  | 'rain'
-  | 'exposure'
-  | 'population'
-  | 'buildings';
+export type RiskAssessmentType = 'rain';
 
-export const RISK_ASSESSMENT: RiskAssessmentType[] = [
-  'rain',
-  'exposure',
-  'population',
-  'buildings',
-];
+export type ExposureType = 'population' | 'buildings';
 
+export const RISK_ASSESSMENT: RiskAssessmentType[] = ['rain'];
+
+export const EXPOSURE_NAMES: ExposureType[] = ['population', 'buildings'];
 @Injectable({
   providedIn: 'root',
 })
 export class RiskAssessmentServicesService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  private EXPOSURE_POPULATION =
+    'https://upri-noah.s3.ap-southeast-1.amazonaws.com/example/PH060400000_FB_Pop.geojson';
+
+  getExposurePopulation() {
+    return this.http.get(`${this.EXPOSURE_POPULATION}`);
+  }
 }
