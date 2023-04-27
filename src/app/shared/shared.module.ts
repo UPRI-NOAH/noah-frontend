f8980b5ca37fecc7f5717a397c25dcfd01e86782
bon-carpo <jccarpo@up.edu.ph>
2023-04-27 15:04:39 +0800

    add summary and needs component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { ReplacePipe } from './pipes/replace.pipe';
 import { ChangeStyleButtonComponent } from './components/change-style-button/change-style-button.component';
 import { RiskAssessmentModalComponent } from './components/risk-assessment-modal/risk-assessment-modal.component';
+import { RiskAssessmentSummaryComponent } from './components/risk-assessment-summary/risk-assessment-summary.component';
+import { RiskAssessmentNeedsComponent } from './components/risk-assessment-needs/risk-assessment-needs.component';
 
 const modules = [CommonModule, FormsModule, ReactiveFormsModule];
 const components = [
@@ -14,6 +16,8 @@ const components = [
   SearchComponent,
   BannerComponent,
   RiskAssessmentModalComponent,
+  RiskAssessmentSummaryComponent,
+  RiskAssessmentNeedsComponent,
 ];
 const directives = [];
 const pipes = [ReplacePipe];
