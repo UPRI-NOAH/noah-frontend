43eca842ee19cf250af70c87b8c54e7075b70377
kennethliporada <62271587+kennethliporada@users.noreply.github.com>
2023-05-03 19:16:10 +0800

    WIP: Seperated component for exposure type

--- src/app/features/noah-playground/components/risk-exposure/risk-exposure.component.ts
+++ src/app/features/noah-playground/components/risk-exposure/risk-exposure.component.ts
@@ -4,7 +4,7 @@ import {
   ExposureType,
   RiskAssessmentType,
 } from '@features/noah-playground/services/risk-assessment.service';
-import { RiskGroupType } from '@features/noah-playground/store/noah-playground.store';
+import { RiskExposureType } from '@features/noah-playground/store/noah-playground.store';
 import { Observable } from 'rxjs';
 
 import { first } from 'rxjs/operators';
@@ -14,55 +14,28 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./risk-exposure.component.scss'],
 })
 export class RiskExposureComponent implements OnInit {
-  riskAssessmentRain: RiskAssessmentType[] = ['rain'];
-  exposureTypes: ExposureType[] = ['population', 'buildings'];
-  @Input() riskType: RiskGroupType;
+  names: RiskExposureType[] = ['population', 'building'];
 
-  shown = false;
-  initialOpacityValue: number = 100;
-
+  selectedRiskExposure$: Observable<RiskExposureType>;
   exposureShown$: Observable<boolean>;
-  selectedExposureType$: Observable<ExposureType>;
-
-  riskGroupShown$: Observable<boolean>;
-
-  checkedRain = false;
-  checkedExp = false;
-  isRadioEnabled = false;
   isCheckedExp = false;
 
   constructor(private pgService: NoahPlaygroundService) {}
 
   ngOnInit(): void {
-    this.pgService
-      .getRisk$(this.riskType)
-      .pipe(first())
-      .subscribe(({ shown, opacity }) => {
-        this.shown = shown;
-        this.initialOpacityValue = opacity;
-      });
+    // for SOLO
+    this.selectedRiskExposure$ = this.pgService.selectedRiskExposure$;
+    this.exposureShown$ = this.pgService.riskExposureShown$;
   }
 
-  selectExposure(type: ExposureType) {
-    this.pgService.selectExposureType(type);
-  }
-
-  toggleRain(events: Event) {
-    events.stopPropagation();
-    events.stopImmediatePropagation();
-    this.checkedRain = (events.target as HTMLInputElement).checked;
-    this.updateRadioEnabled();
+  selectExposure(exposureTypes: RiskExposureType) {
+    this.pgService.selectRiskExposure(exposureTypes);
   }
 
   toggleExposure(events: Event) {
-    this.shown = !this.shown;
-    this.pgService.setRiskSoloShown(this.shown, this.riskType);
-    //this.pgService.toggleExposureTypeVisibility()
-    this.checkedExp = (events.target as HTMLInputElement).checked;
-    this.updateRadioEnabled();
-  }
+    events.stopPropagation();
+    events.stopImmediatePropagation();
 
-  updateRadioEnabled() {
-    this.isRadioEnabled = this.checkedRain && this.checkedExp;
+    this.pgService.toggleRiskExposureGroupVisibility();
   }
 }
