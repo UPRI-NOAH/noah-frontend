5abb0db1802798263f85fb7057cee168638ad679
bon-carpo <jccarpo@up.edu.ph>
2023-08-24 15:38:31 +0800

    display risk assessment in studio

--- src/app/features/noah-playground/components/risk-assessment-group/risk-assessment-group.component.ts
+++ src/app/features/noah-playground/components/risk-assessment-group/risk-assessment-group.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NoahPlaygroundService } from '@features/noah-playground/services/noah-playground.service';
+import { RiskAssessmentType } from '@features/noah-playground/store/noah-playground.store';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'noah-risk-assessment-group',
@@ -6,7 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./risk-assessment-group.component.scss'],
 })
 export class RiskAssessmentGroupComponent implements OnInit {
-  constructor() {}
+  riskAssessmentTypeList: RiskAssessmentType[] = [
+    'rain-forecast',
+    'population',
+  ];
 
-  ngOnInit(): void {}
+  expanded$: Observable<boolean>;
+  shown$: Observable<boolean>;
+
+  constructor(private pgService: NoahPlaygroundService) {}
+
+  ngOnInit(): void {
+    this.expanded$ = this.pgService.riskAssessmentGroupExpanded$.pipe(
+      shareReplay(1)
+    );
+    this.shown$ = this.pgService.riskAssessmentGroupShown$.pipe(shareReplay(1));
+  }
+
+  toggleExpanded(event: Event) {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+
+    this.pgService.toggleRiskAssessmentGroupProperty('expanded');
+  }
+
+  toggleShown(event: Event) {
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+
+    this.pgService.toggleRiskAssessmentGroupProperty('shown');
+  }
 }
--- src/app/features/noah-playground/components/risk-assessment-solo/risk-assessment-solo.component.ts
+++ src/app/features/noah-playground/components/risk-assessment-solo/risk-assessment-solo.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NoahPlaygroundService } from '@features/noah-playground/services/noah-playground.service';
+import {
+  RiskAssessmentType,
+  VolcanoType,
+} from '@features/noah-playground/store/noah-playground.store';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'noah-risk-assessment-solo',
@@ -6,7 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./risk-assessment-solo.component.scss'],
 })
 export class RiskAssessmentSoloComponent implements OnInit {
-  constructor() {}
+  @Input() riskType: RiskAssessmentType;
+  shown = false;
 
-  ngOnInit(): void {}
+  get displayName(): string {
+    return this.riskType.replace('-', ' ');
+  }
+
+  constructor(private pgService: NoahPlaygroundService) {}
+
+  ngOnInit(): void {
+    this.pgService
+      .getRiskAssessment$(this.riskType)
+      .pipe(first())
+      .subscribe(({ shown }) => {
+        this.shown = shown;
+      });
+  }
+
+  toggleShown() {
+    this.shown = !this.shown;
+    this.pgService.setRiskAssessmentSoloShown(this.shown, this.riskType);
+  }
 }
