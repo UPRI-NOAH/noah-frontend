7af76e213a69f3393fc6a3e38d44447aa1908ee5
kennethliporada <62271587+kennethliporada@users.noreply.github.com>
2023-09-18 17:28:46 +0800

    example data for the 4as modal table

--- src/app/features/noah-playground/services/risk-assessment.service.ts
+++ src/app/features/noah-playground/services/risk-assessment.service.ts
@@ -17,9 +17,19 @@ export type AffectedData = {
 export class RiskAssessmentService {
   constructor(private http: HttpClient) {}
 
+  private NGROK_BASE_URL = 'https://e1f6-136-158-11-135.ngrok-free.app';
+
   getAffectedPopulation(): Observable<any> {
     return this.http.get(
       'https://upri-noah.s3.ap-southeast-1.amazonaws.com/4As/prov_rankings.json'
+    );
+  }
+
+  //https://e1f6-136-158-11-135.ngrok-free.app/api/affected_brgy/?affected=yes
+
+  getAffectedPopulations(): Observable<any> {
+    return this.http.get(
+      `${this.NGROK_BASE_URL}/api/affected_brgy/?affected=yes`
     );
   }
 }
