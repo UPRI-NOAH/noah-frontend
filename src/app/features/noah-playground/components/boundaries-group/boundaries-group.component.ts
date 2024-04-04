a6bf5b6716f9838f3e0520bc16619bdefbdb515d
bon-carpo <jccarpo@up.edu.ph>
2024-04-04 15:28:17 +0800

    add boundaries component

---
+++ src/app/features/noah-playground/components/boundaries-group/boundaries-group.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-boundaries-group',
+  templateUrl: './boundaries-group.component.html',
+  styleUrls: ['./boundaries-group.component.scss'],
+})
+export class BoundariesGroupComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/features/noah-playground/components/boundaries-solo/boundaries-solo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-boundaries-solo',
+  templateUrl: './boundaries-solo.component.html',
+  styleUrls: ['./boundaries-solo.component.scss'],
+})
+export class BoundariesSoloComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
