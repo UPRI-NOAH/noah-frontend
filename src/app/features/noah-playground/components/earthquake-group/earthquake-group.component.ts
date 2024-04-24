542865b95d89a051b3cdc13dd9664cff37ebdad1
upri <upri@Bon-MacBook-Pro.local>
2024-04-24 14:31:58 +0800

    add component for earthquake group and solo

---
+++ src/app/features/noah-playground/components/earthquake-group/earthquake-group.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-earthquake-group',
+  templateUrl: './earthquake-group.component.html',
+  styleUrls: ['./earthquake-group.component.scss'],
+})
+export class EarthquakeGroupComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/features/noah-playground/components/earthquake-solo/earthquake-solo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'noah-earthquake-solo',
+  templateUrl: './earthquake-solo.component.html',
+  styleUrls: ['./earthquake-solo.component.scss'],
+})
+export class EarthquakeSoloComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
