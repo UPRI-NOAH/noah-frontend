8bf0665f5c9a1d2fd2f603fab15ff306beeb421f
upri <upri@Bon-MacBook-Pro.local>
2024-05-15 18:09:02 +0800

    change bg color

--- src/app/shared/components/summary-earthquake/summary-earthquake.component.ts
+++ src/app/shared/components/summary-earthquake/summary-earthquake.component.ts
@@ -16,6 +16,7 @@ export class SummaryEarthquakeComponent implements OnInit {
   todayString: string = new Date().toDateString();
   loading = false;
 
+  summaryData: any[] = [];
   summaryRedAlertData: SummaryItem[] = [];
   fetchedRedAlertData: SummaryItem[] = [];
   summaryOrangeAlertData: SummaryItem[] = [];
@@ -78,61 +79,72 @@ export class SummaryEarthquakeComponent implements OnInit {
       .pipe(first())
       .toPromise();
 
-    const locationArr = response.features
-      .filter((a) => a.properties.bldg_name)
-      .map((a) => {
-        return {
-          bldg_name: a.properties.bldg_name,
+    // const locationArr = response.features
+    //   .filter((a) => a.properties.bldg_name)
+    //   .map((a) => {
+    //     return {
+    //       bldg_name: a.properties.bldg_name,
+    //     };
+    //   });
+
+    for (const feature of response.features) {
+      for (const data of feature.properties.data) {
+        const rowData = {
+          bldg_name: feature.properties.bldg_name,
+          rshake_station: data.rshake_station,
+          floor_num: data.floor_num,
         };
-      });
-
-    // const dataArr = response.features
-    // .filter((a) => a.properties.data)
-    // .map((a) => {
-    //   return {
-    //     rshake_station: a.properties.data.rshake_station,
-    //     floor_num: a.properties.data.floor_num,
-    //   };
-    // });
-
-    const res: any = await this.earthquakeService
-      .getEarthquakeSummaryData()
-      .pipe(first())
-      .toPromise();
-
-    const dataArr = res.results.map((a) => {
-      return {
-        acceleration: a.acceleration,
-        displacement: a.displacement,
-        drift: a.drift,
-        intensity: a.intensity,
-      };
-    });
-
-    const newArr = []; //all summary data
-    for (let i = 0; i < locationArr.length; i++) {
-      for (let j = 0; j < dataArr.length; j++) {
-        if (locationArr[i].bldg_name == dataArr[j].drift) {
-          newArr.push({ ...locationArr[i], ...dataArr[j] });
-          break;
-        }
+        this.summaryData.push(rowData);
       }
     }
-
-    const allDataWith = []; //disply all data without null value
-    for (let i = 0; i < newArr.length; i++) {
-      if (newArr[i].bldg_name) {
-        allDataWith.push({ ...newArr[i] });
-      }
-    }
-    const sortName = []; //seperate null value
-    for (let i = 0; i < newArr.length; i++) {
-      if (newArr[i].bldg_name == null) {
-        sortName.push({ ...newArr[i] });
-      }
-    }
-    this.fetchedRedAlertData = allDataWith.concat(sortName);
   }
+
+  // const dataArr = response.features
+  // .filter((a) => a.properties.data)
+  // .map((a) => {
+  //   return {
+  //     rshake_station: a.properties.data.rshake_station,
+  //     floor_num: a.properties.data.floor_num,
+  //   };
+  // });
+
+  // const res: any = await this.earthquakeService
+  //   .getEarthquakeSummaryData()
+  //   .pipe(first())
+  //   .toPromise();
+
+  // const dataArr = res.results.map((a) => {
+  //   return {
+  //     acceleration: a.acceleration,
+  //     displacement: a.displacement,
+  //     drift: a.drift,
+  //     intensity: a.intensity,
+  //   };
+  // });
+
+  // const newArr = []; //all summary data
+  // for (let i = 0; i < locationArr.length; i++) {
+  //   for (let j = 0; j < dataArr.length; j++) {
+  //     if (locationArr[i].bldg_name == dataArr[j].drift) {
+  //       newArr.push({ ...locationArr[i], ...dataArr[j] });
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // const allDataWith = []; //disply all data without null value
+  // for (let i = 0; i < newArr.length; i++) {
+  //   if (newArr[i].bldg_name) {
+  //     allDataWith.push({ ...newArr[i] });
+  //   }
+  // }
+  // const sortName = []; //seperate null value
+  // for (let i = 0; i < newArr.length; i++) {
+  //   if (newArr[i].bldg_name == null) {
+  //     sortName.push({ ...newArr[i] });
+  //   }
+  // }
+  // this.fetchedRedAlertData = allDataWith.concat(sortName);
 
   closeModal() {
     this.modalService.earthquakeSummaryModalClose();
