a92041089f4ca57c56e8c97292d66a8812d69ec4
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2024-08-05 11:00:48 +0800

    Made expanding the scrollbar possible in mobile view

--- src/app/features/noah-playground/pages/noah-playground/noah-playground.component.ts
+++ src/app/features/noah-playground/pages/noah-playground/noah-playground.component.ts
@@ -5,6 +5,11 @@ import { NoahPlaygroundService } from '@features/noah-playground/services/noah-p
 import { QcLoginService } from '@features/noah-playground/services/qc-login.service';
 import { HAZARDS } from '@shared/mocks/hazard-types-and-levels';
 import { Observable } from 'rxjs';
+import {
+  BreakpointObserver,
+  BreakpointState,
+  Breakpoints,
+} from '@angular/cdk/layout';
 
 @Component({
   selector: 'noah-noah-playground',
@@ -19,6 +24,8 @@ export class NoahPlaygroundComponent implements OnInit {
   isSidebarOpen: boolean = false;
   isLogoutAlert: boolean = false;
   isMenu: boolean = true;
+  mobileMenuHeight: '0vh' | '50vh' | '90vh' | '100%' = '50vh';
+  isMobile: boolean = false;
   isList;
   hazardTypes = HAZARDS;
   LoginStatus$: Observable<boolean>;
@@ -37,7 +44,8 @@ export class NoahPlaygroundComponent implements OnInit {
     private pgService: NoahPlaygroundService,
     private title: Title,
     private qcLoginService: QcLoginService,
-    private modalService: ModalService
+    private modalService: ModalService,
+    public breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
@@ -84,6 +92,28 @@ export class NoahPlaygroundComponent implements OnInit {
       this.lagunaAdmin = true;
       this.qcAdmin = false;
     }
+
+    // check if mobile view or desktop view
+
+    this.breakpointObserver
+      .observe([
+        Breakpoints.Small,
+        Breakpoints.WebLandscape,
+        Breakpoints.Medium,
+      ])
+      .subscribe((state) => {
+        const breakpoints = state.breakpoints;
+        if (
+          breakpoints[Breakpoints.WebLandscape] ||
+          breakpoints[Breakpoints.Medium]
+        ) {
+          this.isMobile = false;
+          this.mobileMenuHeight = '100%';
+        } else {
+          this.isMobile = true;
+          this.mobileMenuHeight = '50vh';
+        }
+      });
   }
 
   selectPlace(selectedPlace) {
@@ -124,5 +154,29 @@ export class NoahPlaygroundComponent implements OnInit {
 
   logout(): void {
     this.qcLoginService.logout();
+  }
+
+  enlargeMobileMenu(): void {
+    if (this.mobileMenuHeight === '100%') return;
+
+    this.isMenu = true;
+
+    if (this.mobileMenuHeight === '50vh') {
+      this.mobileMenuHeight = '90vh';
+    }
+  }
+
+  reduceMobileMenu(): void {
+    if (this.mobileMenuHeight === '100%') {
+      this.isMenu = false;
+    } else if (this.mobileMenuHeight === '90vh') {
+      this.mobileMenuHeight = '50vh';
+    } else if (this.mobileMenuHeight === '50vh') {
+      this.isMenu = false;
+    }
+  }
+
+  toggleMenu(): void {
+    this.isMenu = !this.isMenu;
   }
 }
