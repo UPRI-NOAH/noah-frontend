24851dc677ae2398e9bf89887f038bb66902590f
ramoncomendadorjr <90377671+ramoncomendadorjr@users.noreply.github.com>
2024-08-15 09:43:33 +0800

    feat(studio)(qc-iot)(qc-sensor-details)[PWA]: design mobile view for IoT popup details (#398)
    * modified popup for mobile and non mobile view, popup to be moved to click, added necessary functions needed for iot sensor details redesign in .ts
    
    * added text color indicator to the popup which to be made into onclick
    
    * sensor details still on mouseover
    
    * added if condition in mobile, shows sensor details on tap, a graph on double tap, non-mobile view unchanged
    
    * adjusted popup to have space for sensor detail graph, alternative is to double click the plotted sensor to see graph details--for mobile view
    
    * added todo comment
    
    * added graph to popup, made new qcchart function for mobile view
    
    * adjusted default zoom placement of sensor detail popup for mobile view
    
    * updated font family used and font size of sensor details
    
    * reverted popup.removed() for non-mobile screens
    
    * adjusted category text color

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   production: false,
+//   mapbox: {
+//     accessToken:
+//       'pk.eyJ1IjoidXByaS1ub2FoIiwiYSI6ImNrcmExaDdydzRldWYyb21udmw1ejY3ZDYifQ.cs3Ahz2S6fERoI1BAwFO9g',
+//     styles: {
+//       terrain: 'mapbox://styles/upri-noah/ckvnsepq51slc14p1wgy7849a',
+//       satellite: 'mapbox://styles/upri-noah/ckvnso6nd35v514nlx9ib9v9t',
+//     },
+//   },
+//   gaTag: 'G-XXQTRHXBW3',
+// };
+
 export const environment = {
   production: true,
   mapbox: {
