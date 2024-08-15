58d73dadcf2e3f450d8aa129514fbeac20cfaae5
ramoncomendadorjr <90377671+ramoncomendadorjr@users.noreply.github.com>
2024-08-15 11:15:37 +0800

    [WIP] NOAH Website PWA Conversion (#399)
    * ran ng add @angular/pwa@12 which generated necessary files for pwa
    
    * deleted default generated icons from pwa package
    
    * ran pwa-asset-generator package which generated icons, also modified manifest default icons
    
    * updated pwa name and short_name
    
    * updated pwa icons and splash screens
    
    * added install button on top bar, (works only for chrome so far), others will have to browse in the browser's menu to install pwa
    
    * updated splash screen images and put these in a separate folder from pwa-icons, also put meta tags in the index.html
    
    * deleted old generated splash screen images
    
    * changed theme and background color to white
    
    * changed theme and background color to #fafafa in manifest.webmanifest and index.html
    
    * update package in my local
    
    * update env
    
    ---------
    
    Co-authored-by: bon-carpo <jccarpo@up.edu.ph>

--- src/app/features/home/components/navigation/navigation.component.ts
+++ src/app/features/home/components/navigation/navigation.component.ts
@@ -9,7 +9,27 @@ export class NavigationComponent implements OnInit {
   isMenu: boolean = false;
   isList: number;
   isSearch: boolean = false;
+  deferredPrompt: any;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    window.addEventListener('beforeinstallprompt', (event) => {
+      event.preventDefault();
+      this.deferredPrompt = event;
+    });
+  }
+
+  installPWA() {
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+        this.deferredPrompt = null;
+      });
+    }
+  }
 }
