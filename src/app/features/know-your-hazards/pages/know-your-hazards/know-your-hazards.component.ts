581e7d9f1bc980d5cf7aae599aeedbf78a70d6e6
Ken <62271587+kennethliporada@users.noreply.github.com>
2024-09-10 10:18:05 +0800

    fix(kyh-pwa): mobile view sidebar (#405)
    * Added github actions workflow for aut deployment to .72 server.
    
    * Added github action for S3 deployment.
    
    * Changed to staging. Created branch for staging.
    
    * Retrigger deployment workflow.
    
    * fix(studio): zoom in on ios (#192)
    
    # Summary 
    
    - Fixes https://github.com/UPRI-NOAH/noah-frontend/issues/186
    - The `load` event isn't firing on NOAH Studio. As such, the `centerListener()` method isn't called which is responsible for the zooming in to the selected place. However, `style.load` do get called. The fix implemented here is to also use the event `style.load` to call the methods that were previously called by upon the firing of the `load` event but only listen to `style.load` once.
      - We need to further investigate **why** `load` doesn't work on Studio in iOS but works in KYH
    - Confirming that only the NOAH Studio is affected by this issue.
     
    # Demo
    
    ![Kapture 2021-10-05 at 22 49 45](https://user-images.githubusercontent.com/11599005/136047077-d0da7a85-9157-4d01-bae9-07a6039fd9c1.gif)
    
    * ci(netlify): add _redirects file
    
    * S3 Deployment Validation (#198)
    
    * Added if condition to run workflow only on successful PR merge. (#195)
    
    * Checking proper automation workflow. Will revert this small change upon validation. (#196)
    
    * Change deployment trigger. Revert small biblio change. (#199)
    
    * Checking proper automation workflow. Will revert this small change upon validation.
    
    * Changed trigger for deployment workflow. Revert small change to bibliography.
    
    * Cascade dev changes on workflow to prod (#202)
    
    * Added AWS deployment workflow
    
    * changing data for risk assessment delete function for test data
    
    * found trash code
    
    * toggle hide sidebar for mobile and rearrange legend for mobile devices
    
    ---------
    
    Co-authored-by: pfgoting <pfgoting@gmail.com>
    Co-authored-by: Jadurani Davalos <jadurani.davalos@gmail.com>
    Co-authored-by: kennethbeoliporada <kennethbeoliporada@gmail.com>

--- src/app/features/know-your-hazards/pages/know-your-hazards/know-your-hazards.component.ts
+++ src/app/features/know-your-hazards/pages/know-your-hazards/know-your-hazards.component.ts
@@ -7,11 +7,56 @@ import { KyhService } from '@features/know-your-hazards/services/kyh.service';
   styleUrls: ['./know-your-hazards.component.scss'],
 })
 export class KnowYourHazardsComponent implements OnInit {
+  sideBarMobile: boolean = true;
+  btnShowSideBar: boolean = false;
   constructor(private kyhService: KyhService, private title: Title) {}
+
+  kyhLegend: boolean = true;
+  btnLegend: boolean = false;
+  legendHideSide: boolean = false;
+  btnHideSide: boolean = false;
+  minimizeLegendSide: boolean = false;
 
   ngOnInit(): void {
     this.title.setTitle('NOAH - Know Your Hazards');
     this.kyhService.init();
     this.kyhService.setCurrentView('all');
+  }
+
+  hideSideBar() {
+    this.sideBarMobile = false;
+    this.btnShowSideBar = true;
+    this.kyhLegend = false;
+    this.btnLegend = true;
+    this.btnHideSide = true;
+  }
+
+  showSideBarMobile() {
+    this.sideBarMobile = true;
+    this.btnShowSideBar = false;
+
+    this.kyhLegend = true;
+    this.btnLegend = false;
+
+    this.legendHideSide = false;
+    this.minimizeLegendSide = false;
+    this.btnHideSide = false;
+  }
+
+  openLegend() {
+    this.btnLegend = !this.btnLegend;
+    this.kyhLegend = !this.kyhLegend;
+  }
+
+  openLegendHideSide() {
+    this.legendHideSide = true;
+    this.minimizeLegendSide = true;
+    this.btnHideSide = false;
+  }
+
+  hideLegendSide() {
+    this.legendHideSide = false;
+    this.btnHideSide = true;
+    this.minimizeLegendSide = false;
   }
 }
