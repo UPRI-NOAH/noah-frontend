c28333ae452689059a242850345c811f3d5861f6
kennethliporada <62271587+kennethliporada@users.noreply.github.com>
2025-02-04 11:47:52 +0800

    comment access token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
   mapbox: {
+    /*
     accessToken:
-      'pk.eyJ1IjoidXByaS1ub2FoIiwiYSI6ImNrcmExaDdydzRldWYyb21udmw1ejY3ZDYifQ.cs3Ahz2S6fERoI1BAwFO9g',
+       'Put Access token here',
+    */
     styles: {
       terrain: 'mapbox://styles/upri-noah/ckupb1t4ybxq517s530madpso',
       satellite: 'mapbox://styles/upri-noah/ckupb79jmahqr17loikd0hcwi',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,10 @@
 export const environment = {
   production: false,
   mapbox: {
+    /*
     accessToken:
-      'pk.eyJ1IjoidXByaS1ub2FoIiwiYSI6ImNrcmExaDdydzRldWYyb21udmw1ejY3ZDYifQ.cs3Ahz2S6fERoI1BAwFO9g',
+       'Put Access token here',
+    */
     styles: {
       terrain: 'mapbox://styles/upri-noah/ckvnsepq51slc14p1wgy7849a',
       satellite: 'mapbox://styles/upri-noah/ckvnso6nd35v514nlx9ib9v9t',
