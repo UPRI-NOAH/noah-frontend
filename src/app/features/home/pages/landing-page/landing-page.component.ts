97a84939ef39f087d601028f3dd872398f8e3f32
kennethliporada <62271587+kennethliporada@users.noreply.github.com>
2025-04-08 13:36:51 +0800

    landing page carousel for contour

--- src/app/features/home/pages/landing-page/landing-page.component.ts
+++ src/app/features/home/pages/landing-page/landing-page.component.ts
@@ -10,6 +10,9 @@ import { QcSensorService } from '@features/noah-playground/services/qc-sensor.se
 export class LandingPageComponent implements OnInit {
   searchTerm: string;
   isDropdownOpen = false;
+  currentSlide = 0;
+  totalSlides = 2;
+  private slideInterval: any;
 
   constructor(
     private kyhService: KyhService,
@@ -20,6 +23,7 @@ export class LandingPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.title.setTitle('NOAH - Nationwide Operational Assessment of Hazards');
+    this.startAutoSlide();
   }
 
   selectPlace(selectedPlace) {
@@ -28,6 +32,22 @@ export class LandingPageComponent implements OnInit {
     this.kyhService.setCenter({ lat, lng });
     this.kyhService.setCurrentCoords({ lat, lng });
     this.router.navigate(['know-your-hazards']);
+  }
+
+  ngOnDestroy(): void {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  startAutoSlide(): void {
+    this.slideInterval = setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+    }, 5000); // 5 seconds
+  }
+
+  goToSlide(index: number): void {
+    this.currentSlide = index;
   }
 
   gotoTop() {
