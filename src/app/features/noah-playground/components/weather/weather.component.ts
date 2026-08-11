bcfb429016787fe07e46ea0517b3dcf16e035d5f
kennethliporada <62271587+kennethliporada@users.noreply.github.com>
2026-08-11 17:35:16 +0800

    renamed display name NOAA

--- src/app/features/noah-playground/components/weather/weather.component.ts
+++ src/app/features/noah-playground/components/weather/weather.component.ts
@@ -23,7 +23,7 @@ export class WeatherComponent implements OnInit {
 
   get displayName(): string {
     if (this.name === 'NOAA') {
-      return 'NOAA GFS';
+      return 'NOAA Global Forecast System (GFS)';
     }
     return this.name.replace('-', ' + ');
   }
